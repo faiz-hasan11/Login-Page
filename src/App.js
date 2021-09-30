3b3c488715e9234ba42b2a181d07cbfdc5d05c24
faiz-hasan11 <faizhasan2019@iiitkottayam.ac.in>
2021-10-01 03:00:22 +0530

    Designed Login Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Landing from "./components/LandingPage/Landing";
 function App() {
   return (
     <div className="App">
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+      </style>
       <Landing/>
     </div>
   );
--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import "./Landing.css"
-import Google from "../../images/google.png"
-import Facebook from "../../images/facebook.png"
-import Illustration from "../../images/Illustration.jpg"
+import Illustration from "../../images/illus.png"
 const Landing = () => {
 
     const [isSignUp,setIsSignUp] = useState(false)
@@ -11,70 +9,59 @@ const Landing = () => {
         <div className="container">
             <div className="subcontainer">
                 <div className="left-container">
-                    <h1 className="title">
-                        {
-                            isSignUp ? "Sign Up" : "Sign In"
-                        }
-                    </h1>
-                    {
-                        isSignUp ? 
-                            <>
-                                <div className="input-container">
-                                    <label className="label">Email</label>
-                                    <br/>
-                                    <input className="input-field" placeholder="Enter Your Email" type="email"/>
-                                </div>
-                                <div className="input-container">
-                                    <label className="label">Password</label>
-                                    <br/>
-                                    <input className="input-field" placeholder="Enter Your Password" type="password"/>
-                                </div>
-                                <div className="input-container">
-                                    <label className="label">Confirm Password</label>
-                                    <br/>
-                                    <input className="input-field" placeholder="Enter Your Password Again" type="password"/>
-                                </div>
-                            </> 
-                            :
-                            <>
-                                <div className="input-container">
-                                    <label className="label">Email</label>
-                                    <br/>
-                                    <input className="input-field" placeholder="Enter Your Email" type="email"/>
-                                </div>
-                                <div className="input-container">
-                                <label className="label">Password</label>
-                                <br/>
-                                <input className="input-field" placeholder="Enter Your Password" type="password"/>
-                                </div>
-                            </> 
-                    }
-                    {
-                        !isSignUp &&
-                            <div className="forgot">
-                                <p>Forgot your password?</p>
-                            </div>
-                    }
-                    <div className="btn-container">
-                        <button>
+                    <div className="left-sub-container">
+                            <h1 className="title">
                             {
                                 isSignUp ? "Sign Up" : "Sign In"
                             }
-                        </button>
-                    </div>
-                    <div className="or-container">
-                        <p>Or</p>
-                    </div>
-                    <div className="social-container">
-                        <div className="social-single-container">
-                            <img src={Google}/>
-                            <p>Google</p>
+                        </h1>
+                        {
+                            isSignUp ? 
+                                <>
+                                    <div className="input-container">
+                                        <label className="label">Account ID</label>
+                                        <br/>
+                                        <input className="input-field" placeholder="Enter Your Account ID" type="email"/>
+                                    </div>
+                                    <div className="input-container">
+                                        <label className="label">Password</label>
+                                        <br/>
+                                        <input className="input-field" placeholder="Enter Your Password" type="password"/>
+                                    </div>
+                                    <div className="input-container">
+                                        <label className="label">Confirm Password</label>
+                                        <br/>
+                                        <input className="input-field" placeholder="Enter Your Password Again" type="password"/>
+                                    </div>
+                                </> 
+                                :
+                                <>
+                                    <div className="input-container">
+                                        <label className="label">Account ID</label>
+                                        <br/>
+                                        <input className="input-field" placeholder="Enter Your Account ID" type="email"/>
+                                    </div>
+                                    <div className="input-container">
+                                    <label className="label">Password</label>
+                                    <br/>
+                                    <input className="input-field" placeholder="Enter Your Password" type="password"/>
+                                    </div>
+                                </> 
+                        }
+                        {
+                            !isSignUp &&
+                                <div className="forgot">
+                                    <p>Forgot your password?</p>
+                                </div>
+                        }
+                        <div className="btn-container">
+                            <button>
+                                {
+                                    isSignUp ? "Sign Up" : "Sign In"
+                                }
+                            </button>
                         </div>
-                        <div className="social-single-container">
-                            <img src={Facebook}/>
-                            <p>Facebook</p>
-                        </div>
-                    </div>
+                    </div>                    
                     <div className="create-Acc">
                         <p>
                             {
@@ -91,7 +78,11 @@ const Landing = () => {
                     </div>
                 </div>
                 <div className="right-container">
-                    <img src={Illustration}/>
+                    <img 
+                        src={Illustration}
+                        alt="Illustration"
+                    />
+                    
                 </div>
             </div>
         </div>
