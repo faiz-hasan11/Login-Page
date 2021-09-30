82e072122fa372f5c8fafcdf6119b99512a9b8db
faiz-hasan11 <faizhasan2019@iiitkottayam.ac.in>
2021-10-01 04:03:45 +0530

    Added Input Handle Functionality

--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -1,9 +1,96 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Landing.css"
 import Illustration from "../../images/illus.png"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Landing = () => {
 
-    const [isSignUp,setIsSignUp] = useState(false)
+    const [isSignUp,setIsSignUp] = useState(false);
+    const [Id,setId] = useState("");
+    const [password,setPassword] = useState("");
+    const [confirmPassword,setConfirmPassword] = useState("");
+
+    const notify = (text,type) => 
+    {
+        if(type === "warn")
+        {
+                    toast.warn(text, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+        }
+        else if(type === "error")
+        {
+            toast.error(text, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        }
+        else
+        {
+            toast.success(text, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        }
+    }
+
+
+    const handleSignIn = () => {
+        if(isSignUp)
+        {
+            if(Id === "" || password === "" || confirmPassword === "")
+            {
+                notify("Input Field Should Not Be Empty","warn")
+            }
+            else if(password !== confirmPassword)
+            {
+                notify("Password Do Not Match","error")
+            }
+            else
+            {
+                notify("Account Successfully Created","Success");
+                setId("");
+                setPassword("");
+                setConfirmPassword("");
+            }
+        }
+        else
+        {
+            if(Id === "" || password === "")
+            {
+                notify("Input Field Should Not Be Empty","warn")
+            }
+            else
+            {
+                notify("Successfully Logged In","Success");
+                setId("");
+                setPassword("");
+                setConfirmPassword("");
+            }
+        }
+    }
+
+    useEffect(() => {
+            setId("");
+            setPassword("");
+            setConfirmPassword("");
+    },[isSignUp])
 
     return (
         <div className="container">
@@ -21,17 +108,17 @@ const Landing = () => {
                                     <div className="input-container">
                                         <label className="label">Account ID</label>
                                         <br/>
-                                        <input className="input-field" placeholder="Enter Your Account ID" type="email"/>
+                                        <input className="input-field" placeholder="Enter Your Account ID" type="email" value={Id} onChange={(e) => setId(e.target.value)}/>
                                     </div>
                                     <div className="input-container">
                                         <label className="label">Password</label>
                                         <br/>
-                                        <input className="input-field" placeholder="Enter Your Password" type="password"/>
+                                        <input className="input-field" placeholder="Enter Your Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                     </div>
                                     <div className="input-container">
                                         <label className="label">Confirm Password</label>
                                         <br/>
-                                        <input className="input-field" placeholder="Enter Your Password Again" type="password"/>
+                                        <input className="input-field" placeholder="Enter Your Password Again" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                                     </div>
                                 </> 
                                 :
@@ -39,12 +126,12 @@ const Landing = () => {
                                     <div className="input-container">
                                         <label className="label">Account ID</label>
                                         <br/>
-                                        <input className="input-field" placeholder="Enter Your Account ID" type="email"/>
+                                        <input className="input-field" placeholder="Enter Your Account ID" type="email" value={Id} onChange={(e) => setId(e.target.value)}/>
                                     </div>
                                     <div className="input-container">
                                     <label className="label">Password</label>
                                     <br/>
-                                    <input className="input-field" placeholder="Enter Your Password" type="password"/>
+                                    <input className="input-field" placeholder="Enter Your Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                     </div>
                                 </> 
                         }
@@ -55,7 +142,7 @@ const Landing = () => {
                                 </div>
                         }
                         <div className="btn-container">
-                            <button>
+                            <button onClick={handleSignIn}>
                                 {
                                     isSignUp ? "Sign Up" : "Sign In"
                                 }
@@ -85,6 +172,7 @@ const Landing = () => {
                     
                 </div>
             </div>
+            <ToastContainer />
         </div>
     )
 }
